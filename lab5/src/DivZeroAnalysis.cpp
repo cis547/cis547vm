c177e13635faf5482feaf1d7e61f8a073796c404
Aaditya Naik <aadi.naik@gmail.com>
2021-06-14 13:14:09 -0400

    Added modifications to the lab 5 skeleton

--- lab5/src/DivZeroAnalysis.cpp
+++ lab5/src/DivZeroAnalysis.cpp
@@ -24,45 +24,25 @@ namespace dataflow {
 
 // This function can used to evaluate Instruction::PHI */
 Domain *evalPhiNode(PHINode *PHI, const Memory *Mem) {
-  /* Add your code here */
-  Value* cv = PHI->hasConstantValue();
-  if(cv){
-    // Add your code here
-    // eval cv, manipulate Mem, return
-  }
-  unsigned int n = PHI->getNumIncomingValues();
-  Domain* joined = NULL;
-  for(unsigned int i = 0; i < n; i++){
-    // Add your code here
-    // eval PHI->getIncomingValue(i), translate it to a Domain, manipulate Mem
-    Domain* V = NULL;
-    if(!joined){
-      joined = V;
-    }
-    joined = Domain::join(joined, V);
-  }
-  return joined;
+  /* Add your code from Lab 4 here */
 }
 
 
 // This function is intended to return the union of two Memory objects (M1 and M2), accounting for Domain values
 Memory* join(Memory *M1, Memory *M2) {
-  /* Add your code here */
-  Memory* Result = new Memory();
-  return Result;
+  /* Add your code from Lab 4 here */
 }
 
 
 // Return true if the two memories M1 and M2 are strictly equal
 bool equal(Memory *M1, Memory *M2) {
-  //* Add your code here */
-  return true;
+  //* Add your code from Lab 4 here */
 }
 
 
 // Flow abstract domain from all predecessors of I into the In Memory object for I.
 void DivZeroAnalysis::flowIn(Instruction *I, Memory *In) {
-  /* Add your code here */
+  /* Add your code from Lab 4 here */
 }
 
 
@@ -70,13 +50,47 @@ void DivZeroAnalysis::flowIn(Instruction *I, Memory *In) {
 void DivZeroAnalysis::transfer(Instruction *I, const Memory *In, Memory *NOut,
                                PointerAnalysis *PA,
                                SetVector<Value *> PointerSet) {
-  /* Add your code here */
+  if (AllocaInst *AI = dyn_cast<AllocaInst>(I)) {
+    // do nothing, this is taken care of in PointerAnalysis.cpp
+    return;
+  } else if (StoreInst *SI = dyn_cast<StoreInst>(I)) {
+    // If the value being stored is not an integer, then don't do anything
+    if(!SI->getValueOperand()->getType()->isIntegerTy())
+      return;
+    
+    /**
+     * Otherwise:
+     * 1. Evaluate the domain D for SI
+     * 2. Get all the pointers that "may-alias" the SI from PA and PointerSet
+     * 3. Get the domain of each may-alias of SI from In, and join all those domains as well as domain D
+     * 4. Update the domain for SI and each may-aliasing pointer to the new joined domain in NOut
+     */
+  } else if (LoadInst *LI = dyn_cast<LoadInst>(I)) {
+    // if the value being loaded is not of an integer type, then do nothing
+    if(!LI->getType()->isIntegerTy())
+      return;
+
+    /**
+     * Otherwise:
+     * 1. Get the pointer operand P of the load instruction (there is a specific getPointerOperand function, check the documentation for details)
+     * 2. If P is in In, then update the domain of LI in NOut to the domain of P
+     * 3. Otherwise, update the domain of LI in NOut to MaybeZero
+     */
+  } else if (ReturnInst *RI = dyn_cast<ReturnInst>(I)) {
+    // do nothing
+    return;
+  } else if (BranchInst *BI = dyn_cast<BranchInst>(I)) {
+    // do nothing
+    return;
+  } else {
+    // add your code from Lab 4 here
+  }
 }
 
 
 // For a given instruction, check if the pre-transfer memory and post-transfer memory are equal, and update WorkSet as needed
 void DivZeroAnalysis::flowOut(Instruction *I, Memory *Pre, Memory *Post, SetVector <Instruction *> &WorkSet) {
-  /* Add your code here */
+  /* Add your code from Lab 4 here */
 }
 
 
@@ -88,7 +102,7 @@ void DivZeroAnalysis::doAnalysis(Function &F, PointerAnalysis *PA) {
     PointerSet.insert(&(*I));
   }
 
-  /* Add your code here */
+  /* Add your code from Lab 4 here */
   /* Basic Workflow-
        Visit instruction in WorkSet
        For each visited instruction I, construct its In memory by joining all memory sets of incoming flows (predecessors of I)
@@ -101,7 +115,7 @@ void DivZeroAnalysis::doAnalysis(Function &F, PointerAnalysis *PA) {
 
 // Check if a specific instruction can incur a divide-by-zero error
 bool DivZeroAnalysis::check(Instruction *I) {
-  /* Add your code here */
+  /* Add your code from Lab 4 here */
   return false;
 }
 
@@ -110,3 +124,4 @@ char DivZeroAnalysis::ID = 1;
 static RegisterPass<DivZeroAnalysis> X("DivZero", "Divide-by-zero Analysis",
                                        false, false);
 } // namespace dataflow
+
