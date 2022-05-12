c7b715713149eff5a687e7b1b20958bac296fd6a
Harsh Parekh <harsh_parekh@outlook.com>
2022-05-11 21:38:48 -0400

    Fix typo 1 → 2.

--- warmup/src/User.cpp
+++ warmup/src/User.cpp
@@ -9,5 +9,5 @@ std::string User::getEmail() {
 }
 
 bool operator==(const User& user1, const User& user2) {
-    return user1.ID == user2.ID && user1.email == user2.email && user1.name == user1.name;
+    return user1.ID == user2.ID && user1.email == user2.email && user1.name == user2.name;
 }
