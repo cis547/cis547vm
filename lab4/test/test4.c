0ffb65714400de735cf67c3378307f1b7bc86f5a
Harsh Parekh <harsh_parekh@outlook.com>
2022-05-31 19:23:52 -0400

    Init lab4

---
+++ lab4/test/test4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  int i;
+  int x = 0;
+  int y = 2;
+	int z;
+	while ((i = getchar()) - '0' >= 0) {
+    if (i == 'y') {
+      z = y / x;
+		}
+  }
+  return 0;
+}
---
+++ lab4/test/test5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  int i;
+  int j = 0;
+  int x = 0;
+  int y = 2;
+	int z;
+	while ((i = getchar()) - '0' >= 0) {
+    if (i == 'y') {
+      j = 1;
+		}
+    if (j == 1 && i == 'x') {
+      z = y / x;
+    }
+  }
+  return 0;
+}
