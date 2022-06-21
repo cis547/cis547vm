2f67d2d06df63e55ee95d7e375d8c5eb70c80a13
Harsh Parekh <harsh_parekh@outlook.com>
2022-06-21 01:20:11 -0400

    Add lab6

---
+++ lab6/src/Transfer.cpp
@@ -0,0 +1,125 @@
+#include "DivZeroAnalysis.h"
+#include "Utils.h"
+
+namespace dataflow {
+
+/**
+ * @brief Is the given instruction a user input?
+ *
+ * @param Inst The instruction to check.
+ * @return true If it is a user input, false otherwise.
+ */
+bool isInput(Instruction *Inst) {
+  if (auto Call = dyn_cast<CallInst>(Inst)) {
+    if (auto Fun = Call->getCalledFunction()) {
+      return (Fun->getName().equals("getchar") ||
+              Fun->getName().equals("fgetc"));
+    }
+  }
+  return false;
+}
+
+/**
+ * Evaluate a PHINode to get its Domain.
+ *
+ * @param Phi PHINode to evaluate
+ * @param InMem InMemory of Phi
+ * @return Domain of Phi
+ */
+Domain *eval(PHINode *Phi, const Memory *InMem) {
+  if (auto ConstantVal = Phi->hasConstantValue()) {
+    return new Domain(extractFromValue(ConstantVal));
+  }
+
+  Domain *Joined = new Domain(Domain::Uninit);
+
+  for (unsigned int i = 0; i < Phi->getNumIncomingValues(); i++) {
+    auto Dom = getOrExtract(InMem, Phi->getIncomingValue(i));
+    Joined = Domain::join(Joined, Dom);
+  }
+  return Joined;
+}
+
+/**
+ * @brief Evaluate the +, -, * and / BinaryOperator instructions
+ * using the Domain of its operands and return the Domain of the result.
+ *
+ * @param BinOp BinaryOperator to evaluate
+ * @param InMem InMemory of BinOp
+ * @return Domain of BinOp
+ */
+Domain *eval(BinaryOperator *BinOp, const Memory *InMem) {
+  /**
+   * TODO: Write your code here that evaluates +, -, * and /
+   * based on the Domains of the operands.
+   */
+}
+
+/**
+ * @brief Evaluate Cast instructions.
+ *
+ * @param Cast Cast instruction to evaluate
+ * @param InMem InMemory of Instruction
+ * @return Domain of Cast
+ */
+Domain *eval(CastInst *Cast, const Memory *InMem) {
+  /**
+   * TODO: Write your code here to evaluate Cast instruction.
+   */
+  return NULL;
+}
+
+/**
+ * @brief Evaluate the ==, !=, <, <=, >=, and > Comparision operators using
+ * the Domain of its operands to compute the Domain of the result.
+ *
+ * @param Cmp Comparision instruction to evaluate
+ * @param InMem InMemory of Cmp
+ * @return Domain of Cmp
+ */
+Domain *eval(CmpInst *Cmp, const Memory *InMem) {
+  /**
+   * TODO: Write your code here that evaluates:
+   * ==, !=, <, <=, >=, and > based on the Domains of the operands.
+   *
+   * NOTE: There is a lot of scope for refining this, but you can just return
+   * MaybeZero for comparisons other than equality.
+   */
+   return NULL;
+}
+
+void DivZeroAnalysis::transfer(Instruction *Inst, const Memory *In,
+                               Memory &NOut) {
+  if (isInput(Inst)) {
+    // The instruction is a user controlled input, it can have any value.
+    NOut[variable(Inst)] = new Domain(Domain::MaybeZero);
+  } else if (auto Phi = dyn_cast<PHINode>(Inst)) {
+    // Evaluate PHI node
+    NOut[variable(Phi)] = eval(Phi, In);
+  } else if (auto BinOp = dyn_cast<BinaryOperator>(Inst)) {
+    // Evaluate BinaryOperator
+    NOut[variable(BinOp)] = eval(BinOp, In);
+  } else if (auto Cast = dyn_cast<CastInst>(Inst)) {
+    // Evaluate Cast instruction
+    NOut[variable(Cast)] = eval(Cast, In);
+  } else if (auto Cmp = dyn_cast<CmpInst>(Inst)) {
+    // Evaluate Comparision instruction
+    NOut[variable(Cmp)] = eval(Cmp, In);
+  } else if (auto Alloca = dyn_cast<AllocaInst>(Inst)) {
+    // Used for the next lab, do nothing here.
+  } else if (auto Store = dyn_cast<StoreInst>(Inst)) {
+    // Used for the next lab, do nothing here.
+  } else if (auto Load = dyn_cast<LoadInst>(Inst)) {
+    // Used for the next lab, do nothing here.
+  } else if (auto Branch = dyn_cast<BranchInst>(Inst)) {
+    // Analysis is flow-insensitive, so do nothing here.
+  } else if (auto Call = dyn_cast<CallInst>(Inst)) {
+    // Analysis is intra-procedural, so do nothing here.
+  } else if (auto Return = dyn_cast<ReturnInst>(Inst)) {
+    // Analysis is intra-procedural, so do nothing here.
+  } else {
+    errs() << "Unhandled instruction: " << *Inst << "\n";
+  }
+}
+
+} // namespace dataflow
