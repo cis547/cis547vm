3b3e3a6e8ff15def2aa6c6f581901b31b0b99cbd
Zain K Aamer <ZippeyKeys12@gmail.com>
2025-08-26 00:15:40 -0400

    Update Lab 1

--- lab1/c_programs/test1.c
+++ lab1/c_programs/test1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int a = getchar() - '0';
-    int b = getchar() - '0';
+  int a = getchar() - '0';
+  int b = getchar() - '0';
 
-    b *= a;
-    a = b / a;
-    return 0;
+  b *= a;
+  a = b / a;
+  return 0;
 }
---
+++ lab1/c_programs/test11.c
@@ -0,0 +1,41 @@
+#include <setjmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SHUFFLE(a) ((unsigned)a << 7) | ((unsigned)a >> 25)
+
+int hash(int x) {
+  int y = ~x;
+
+  const int MASK_A = 0xF03AD267;
+  y = y ^ MASK_A;
+
+  unsigned u = SHUFFLE(y);
+
+  int w = (y ^ (u << 1)) ^ (u << 1);
+  w = ((w & u) | (w & ~u)) ^ 0;
+
+  for (int i = 0; i < 4; ++i) {
+    w = ((unsigned)w << 1) ^ (w >> 1);
+    w ^= (i * 65342);
+    w &= ~0;
+  }
+
+  unsigned v = w ^ u;
+  v = ((v ^ w) >> 7) | ((unsigned)(v ^ w) << (32 - 7));
+
+  y = ~(v ^ MASK_A);
+
+  y = ((y | 0) & ~0);
+
+  return y;
+}
+
+int main() {
+  int l = 0;
+  int r = hash((getchar() - '0') | 1);
+  int w = l / ((unsigned)r << 2);
+
+  return 0;
+}
